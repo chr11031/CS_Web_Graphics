db600c8d5c407e629e3f77f582add302140a3dc8
dchristensen <dchristensen@mobiusimaging.com>
2017-03-06 14:24:36 -0700

    Mousebased Camera Rotation

--- demo.js
+++ demo.js
@@ -1,84 +1,27 @@
 var camera, scene, renderer;
 var geometry, material, mesh;
 
+
+document.getElementById("requestButton").addEventListener("click", request);
+
 init();
 animate();
 
 function init() {
-
-	/*
-	someGeometry = new THREE.Geometry();
-	geometry.vertices.push(
-		new THREE.Vector3(-200, 200, 0),
-		new THREE.Vector3(-200,-200, 0),
-		new THREE.Vector3( 200,-200, 0),		
-		new THREE.Vector3( 200, 200, 0)		
-	);
-	geometry.faces.push(new THREE.Face3(0, 1, 2));
-	geometry.faces.push(new THREE.Face3(2, 3, 0));
-	geometry.computeFaceNormals();
 	
-	basic = new THREE.MeshNormalMaterial();	
-	var tmpMesh = new THREE.Mesh(someGeometry, basic);
-	tmpP.rotation.x = Math.PI/4;
-	scene.add(tmpMesh);
-	*/
-
-	/*
-	
-	// Needed with shaders
-	var attributes = {
-		displacement: {
-			type: 'f', // a float
-			value: []  // an empty array			
-		}		
-	};
-	
-	// Vertex Shader
-	var vShader = `
-		varying vec3 vNormal;
-		
-		void main() {
-			
-			vNormal = normal;
-			gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-		}	
-	`;
-
-	// Fragment Shader
-	var fShader = `
-		varying vec3 vNormal;
-		
-		void main() {
-			
-			vec3 light = vec3(0.5, 0.2, 1.0);
-			light = normalize(light);
-			
-			float dProd = max(0.0, dot(vNormal, light));
-			
-			gl_FragColor = vec4(dProd, dProd, dProd, 1.0);			
-		}
-	`;
-
-	var shaderMaterial = new THREE.MeshShaderMaterial(
-		{
-			attributes: attributes,
-			vertexShader: vShader,
-			fragmentShader: fShader		
-		}
-	);
-	*/
-
-	
+	// Setup scene
 	dimX = 300;
 	dimY = 300;
-
     camera = new THREE.PerspectiveCamera(75, dimX / dimY, 1, 10000);
     camera.position.z = 1000;
     scene = new THREE.Scene();
 
-	
+	// Setup Light source
+	var light = new THREE.DirectionalLight(0xffffff,1);
+	light.position.set(300,300,1000).normalize();
+	scene.add(light);
 
+	
     geometry = new THREE.CubeGeometry(200, 200, 200);
 	meshPhongMaterial = new THREE.MeshPhongMaterial( 
 		{
@@ -88,22 +31,15 @@ function init() {
 		} 
 	);
 
+	// Arbitrary geometry
     mesh = new THREE.Mesh(geometry, meshPhongMaterial);
 	scene.add(mesh);
-
-	
-	// Light source
-	var light = new THREE.DirectionalLight(0xffffff,1);
-	light.position.set(300,300,1000).normalize();
-	scene.add(light);
-
 	
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(dimX, dimY);
 	renderer.domElement.id = "mainCanvas";
 	
     document.body.appendChild(renderer.domElement);
-
 }
 
 function animate() {
@@ -116,15 +52,16 @@ function ajaxCallBack() {
 	
 	if(this.readyState == 4 && this.status == 200) 
 	{
-
-
 		alert("VALUE");
+	}
+	else 
+	{
+		alert("FAILED TO CONNECT");
 	}
 }
 
 function request() {
 	
-	alert("!!");
 	xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = ajaxCallBack;
 	
@@ -132,34 +69,52 @@ function request() {
 	xhttp.send();	
 }
 
+var mouseX = null;
+var mouseY = null;
 
-function keydown(e) {
-
-	var keyCode = e.keyCode;
-	
-	switch(keyCode) {
-		case 87:
-			mesh.rotation.x -= 0.03;
-			break;
-		case 65:
-			mesh.rotation.y -= 0.03;		
-			break;		
-		case 83:		
-			mesh.rotation.x += 0.03;
-			break;
-		case 68:		
-			mesh.rotation.y += 0.03;		
-			break;
+function touch(e) 
+{
+	if(mouseX == null && mouseY == null)
+	{
+		// Update Coordinates
+		mouseX = e.pageX;
+		mouseY = e.pageY;				
 	}
-	
-	// For Camera rotation when appropriate
-	//camera.rotation.y += diffX * (0.01/Math.PI);
-	//camera.rotation.x += diffY * (0.01/Math.PI);
+	else
+	{
+		deactivateDrag();		
+	}	
 }
 
-window.addEventListener("keydown", keydown);
+function deactivateDrag() 
+{
+	mouseX = null;
+	mouseY = null;
+}
 
+function drag(e) 
+{	
+	var newX = e.pageX;
+	var newY = e.pageY;
+	var diffX = 0;
+	var diffY = 0;	
+	
+	if(mouseX != null && mouseY != null)
+	{
+		diffX = mouseY - newY;		
+		diffY = mouseX - newX;
+		camera.rotation.x += (diffX * 0.01 /Math.PI);
+		camera.rotation.y += (diffY * 0.01 /Math.PI);
 
+		// Update Coordinates
+		mouseX = newX;
+		mouseY = newY;
+	}
+}
+
+document.getElementById("mainCanvas").addEventListener("click", touch);
+document.getElementById("mainCanvas").addEventListener("mousemove", drag);
+document.getElementById("mainCanvas").addEventListener("mouseleave", deactivateDrag);
 
 
 
